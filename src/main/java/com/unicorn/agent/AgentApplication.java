b7784570945a4079c381c2d5de2c8cf9489a5510
dhrubo55 <insular55@gmail.com>
2025-09-04 13:14:59 +0000

    Add Rest Controller

--- src/main/java/com/unicorn/agent/AgentApplication.java
+++ src/main/java/com/unicorn/agent/AgentApplication.java
@@ -16,24 +16,24 @@ public class AgentApplication {
 		SpringApplication.run(AgentApplication.class, args);
 	}
 
-	@Bean
-    public CommandLineRunner cli(ChatClient.Builder chatClientBuilder) {
-        return args -> {
-            var chatClient = chatClientBuilder
-                .defaultSystem("You are a Unicorn Rentals Agent, expert in all sorts of things related to Unicorns and renting them.")
-                .build();
+	// @Bean
+    // public CommandLineRunner cli(ChatClient.Builder chatClientBuilder) {
+    //     return args -> {
+    //         var chatClient = chatClientBuilder
+    //             .defaultSystem("You are a Unicorn Rentals Agent, expert in all sorts of things related to Unicorns and renting them.")
+    //             .build();
 
-            System.out.println("\nI am your Unicorn Rentals assistant.\n");
-            try (Scanner scanner = new Scanner(System.in)) {
-                while (true) {
-                    System.out.print("\nUSER: ");
-                    System.out.println("\nASSISTANT: " +
-                        chatClient.prompt(scanner.nextLine()) // Get the user input
-                            .call()
-                            .content());
-                }
-            }
-        };
-    }
+    //         System.out.println("\nI am your Unicorn Rentals assistant.\n");
+    //         try (Scanner scanner = new Scanner(System.in)) {
+    //             while (true) {
+    //                 System.out.print("\nUSER: ");
+    //                 System.out.println("\nASSISTANT: " +
+    //                     chatClient.prompt(scanner.nextLine()) // Get the user input
+    //                         .call()
+    //                         .content());
+    //             }
+    //         }
+    //     };
+    // }
 
 }
